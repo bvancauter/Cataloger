5f962a4d0a41960228f1d3c79310e080c9d4d4ea
bvancauter <boris.vancauter@gmail.com>
2026-07-22 10:25:34 +0200

    feat: configure FastEndpoints and Swagger

--- Cataloger.Api/src/Cataloger.Api/Program.cs
+++ Cataloger.Api/src/Cataloger.Api/Program.cs
@@ -1,6 +1,19 @@
+using FastEndpoints;
+
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddFastEndpoints();
+
+builder.Services.AddSwaggerDocument();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseFastEndpoints();
+
+app.UseOpenApi();
+
+app.UseSwaggerUi();
 
 app.Run();
