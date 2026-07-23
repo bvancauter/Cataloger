80835b90d087ab20a89f867bdd3550294b7fa930
bvancauter <boris.vancauter@gmail.com>
2026-07-23 06:21:49 +0200

    refactor: replace person pseudonym with full name

--- Cataloger.Api/src/Cataloger.Api/Entities/Books/PersonEntity.cs
+++ Cataloger.Api/src/Cataloger.Api/Entities/Books/PersonEntity.cs
@@ -3,7 +3,8 @@
         public Guid Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string? Pseudonym { get; set; }
+        
+        public string FullName => $"{FirstName} {LastName}";
 
         public ICollection<BookContributorEntity>? Books { get; set; }
     }
