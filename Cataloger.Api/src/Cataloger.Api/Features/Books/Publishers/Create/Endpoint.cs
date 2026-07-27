f41a3692a4da83f8a3ea361f04b636a35245ada0
bvancauter <boris.vancauter@gmail.com>
2026-07-27 08:42:40 +0200

    feat: add an endpoint to create a publisher

---
+++ Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/Create/Endpoint.cs
@@ -0,0 +1,41 @@
+﻿using Cataloger.Api.Data;
+using Cataloger.Api.Entities.Books;
+using Cataloger.Api.Features.Books.Publishers.GetAll;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cataloger.Api.Features.Books.Publishers.Create {
+    public class Endpoint(ApplicationDbContext applicationDbContext) 
+        : Endpoint<Request, Response> {
+
+        public override void Configure() {
+            Post("/api/books/publishers");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(Request request, CancellationToken ct) {
+            var exists = await applicationDbContext.Publishers
+                .AnyAsync(p => p.Name == request.Name, ct);
+
+            if(exists) {
+                AddError(r => r.Name, "Publisher already exists.");
+                await Send.ErrorsAsync(cancellation: ct);
+            }
+            else {
+                var entity = ToEntity(request);
+                applicationDbContext.Publishers.Add(entity);
+                await applicationDbContext.SaveChangesAsync(ct);
+                await Send.OkAsync(ToResponse(entity), cancellation: ct); // need to be modified to send a created code
+            }
+        }
+
+        private static PublisherEntity ToEntity(Request request) => new() {
+            Name = request.Name.Trim()
+        };
+
+        private static Response ToResponse(PublisherEntity entity) => new() {
+            Id = entity.Id,
+            Name = entity.Name
+        };
+    }
+}
---
+++ Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/Create/Models.cs
@@ -0,0 +1,10 @@
+﻿namespace Cataloger.Api.Features.Books.Publishers.Create {
+    public class Request {
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class Response {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
