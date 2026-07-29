3338266b6b09faabfeb758f7de55be98097706e0
bvancauter <boris.vancauter@gmail.com>
2026-07-29 09:01:51 +0200

    feat: add an endpoint to retrieve a publisher

---
+++ Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/Get/Endpoint.cs
@@ -0,0 +1,34 @@
+﻿using Cataloger.Api.Data;
+using Cataloger.Api.Entities.Books;
+using Cataloger.Api.Routes;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cataloger.Api.Features.Books.Publishers.Get {
+    public class Endpoint(ApplicationDbContext applicationDbContext)
+        : Endpoint<Request, Response> {
+
+        public override void Configure() {
+            Get(BookRoutes.Publishers + "/{id:guid}");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(Request req, CancellationToken ct) {
+            var publisher = await applicationDbContext.Publishers
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == req.Id, ct);
+
+            if(publisher is null) {
+                await Send.NotFoundAsync(ct);
+            }
+            else {
+                await Send.OkAsync(ToResponse(publisher), ct);
+            }
+        }
+
+        private static Response ToResponse(PublisherEntity entity) => new() {
+            Id = entity.Id,
+            Name = entity.Name
+        };
+    }
+}
---
+++ Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/Get/Models.cs
@@ -0,0 +1,10 @@
+﻿namespace Cataloger.Api.Features.Books.Publishers.Get {
+    public class Request {
+        public Guid Id { get; set; }
+    }
+
+    public class Response {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
