df35ceda98cd3adb761879d5e3138d9dc4bef2f5
bvancauter <boris.vancauter@gmail.com>
2026-08-13 08:59:59 +0200

    feat: add pagination request validation

---
+++ Cataloger.Api/src/Cataloger.Api/Features/Common/Models/Validators/PagedRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Cataloger.Api.Features.Common.Models.Validators {
+    public class PagedRequestValidator : Validator<PagedRequest> {
+        public PagedRequestValidator() {
+            RuleFor(x => x.Page)
+                .GreaterThanOrEqualTo(1);
+
+            RuleFor(x => x.PageSize)
+                .InclusiveBetween(1, 100);
+        }
+    }
+}
