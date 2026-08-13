fbd52a98fa724b65deea6d3602923364533d53a6
bvancauter <boris.vancauter@gmail.com>
2026-08-13 08:54:08 +0200

    feat: add common pagination models

---
+++ Cataloger.Api/src/Cataloger.Api/Features/Common/Models/PagedRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Cataloger.Api.Features.Common.Models {
+    public class PagedRequest {
+        public int Page { get; init; } = 1;
+        public int PageSize { get; init; } = 12;
+    }
+}
---
+++ Cataloger.Api/src/Cataloger.Api/Features/Common/Models/PagedResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Cataloger.Api.Features.Common.Models {
+    public class PagedResponse<T> {
+        public IReadOnlyCollection<T> Items { get; init; } = [];
+
+        public int Page { get; init; }
+
+        public int PageSize { get; init; }
+
+        public int TotalItems { get; init; }
+
+        public int TotalPages { get; init; }
+
+        public bool HasPrevious => Page > 1;
+
+        public bool HasNext => Page < TotalPages;
+    }
+}
