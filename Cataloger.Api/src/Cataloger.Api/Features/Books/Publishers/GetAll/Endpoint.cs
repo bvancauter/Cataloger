1db789e7c069ab63b3e30de5abe1300ed5bfbdbd
bvancauter <boris.vancauter@gmail.com>
2026-08-13 09:07:42 +0200

    feat: add pagination to publisher listing

--- Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/GetAll/Endpoint.cs
+++ Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/GetAll/Endpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Cataloger.Api.Data;
 using Cataloger.Api.Features.Books.Publishers.Models;
 using Cataloger.Api.Features.Books.Publishers.Utils;
+using Cataloger.Api.Features.Common.Models;
 using Cataloger.Api.Routes;
 using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
@@ -8,17 +9,36 @@ using Microsoft.EntityFrameworkCore;
 namespace Cataloger.Api.Features.Books.Publishers.GetAll;
 
 public class Endpoint(ApplicationDbContext applicationDbContext)
-    : EndpointWithoutRequest<IEnumerable<PublisherListModel>> {
+    : Endpoint<PagedRequest, PagedResponse<PublisherListModel>> {
     public override void Configure() {
         Get(BookRoutes.Publishers);
         AllowAnonymous();
     }
 
-    public override async Task HandleAsync(CancellationToken ct) {
-        var publishers = await applicationDbContext.Publishers
+    public override async Task HandleAsync(PagedRequest request, CancellationToken ct) {
+        var query = applicationDbContext.Publishers
             .AsNoTracking()
+            .OrderBy(x => x.Name);
+
+        var totalItems = await query.CountAsync(ct);
+
+        var publishers = await query
+            .Skip((request.Page - 1) * request.PageSize)
+            .Take(request.PageSize)
             .ToListAsync(ct);
 
-        await Send.OkAsync(publishers.Select(PublisherMapper.ToListModel), ct);
+        var totalPages = (int) Math.Ceiling(totalItems / (double) request.PageSize);
+
+        var response = new PagedResponse<PublisherListModel> {
+            Items = publishers
+                .Select(PublisherMapper.ToListModel)
+                .ToList(),
+            Page = request.Page,
+            PageSize = request.PageSize,
+            TotalItems = totalItems,
+            TotalPages = totalPages
+        };
+
+        await Send.OkAsync(response, ct);
     }
 }
