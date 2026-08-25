21636c771a4278cee4140a4cc984e4203181a378
bvancauter <boris.vancauter@gmail.com>
2026-08-25 08:45:50 +0200

    feat: add error messages in the validator

--- Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/Utils/Validators/PublisherCreateValidator.cs
+++ Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/Utils/Validators/PublisherCreateValidator.cs
@@ -1,19 +1,15 @@
-using Cataloger.Api.Data;
 using Cataloger.Api.Features.Books.Publishers.Models;
 using FastEndpoints;
 using FluentValidation;
 
 namespace Cataloger.Api.Features.Books.Publishers.Utils.Validators {
     public class PublisherCreateValidator : Validator<PublisherCreateModel> {
-        private readonly ApplicationDbContext dbContext;
-
-        public PublisherCreateValidator(ApplicationDbContext dbContext) {
-            this.dbContext = dbContext;
-
+        public PublisherCreateValidator() {
             RuleFor(x => x.Name)
-                .NotNull()
                 .NotEmpty()
-                .MaximumLength(255);
+                .WithMessage("Publisher's name is required!")
+                .MaximumLength(255)
+                .WithMessage("Publisher's name cannot exceed 255 characters!");
         }
     }
 }
