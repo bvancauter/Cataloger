72a69eed6247a5681f512b52712f76a9275aa8d9
bvancauter <boris.vancauter@gmail.com>
2026-08-25 08:50:49 +0200

    feat: add update validator

---
+++ Cataloger.Api/src/Cataloger.Api/Features/Books/Publishers/Utils/Validators/PublisherUpdateValidator.cs
@@ -0,0 +1,18 @@
+﻿using Cataloger.Api.Features.Books.Publishers.Models;
+using FastEndpoints;
+using FluentValidation;
+namespace Cataloger.Api.Features.Books.Publishers.Utils.Validators {
+    public class PublisherUpdateValidator : Validator<PublisherUpdateModel> {
+        public PublisherUpdateValidator() {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Publisher's ID is required!");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Publisher's name is required!")
+                .MaximumLength(255)
+                .WithMessage("Publisher's name cannot exceed 255 characters!");
+        }
+    }
+}
